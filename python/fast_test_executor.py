96efa83f8eec9c9783a041c2ad3fa23932ca13fd
Matt Wiley <mattwiley+gitlab@fastmail.com>
2021-09-10 20:27:20 -0400

    Added upload speed, data sizes, and client information to the output.

--- python/fast_test_executor.py
+++ python/fast_test_executor.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 
 from __future__ import unicode_literals
-
-import os
-from os.path import exists
-
-import sys
-
 import time
 import datetime
-import random
 import json
 
 from selenium import webdriver
-import selenium
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
@@ -37,17 +29,31 @@ def main():
     driver = ChromeWebDriver().get_driver()
     start = datetime.datetime.now()
     driver.get('https://fast.com')
+    time.sleep(1)
+    driver.execute_script("""
+const settings = {
+    "minDuration": "5",
+    "measureUploadLatency": "true",
+    "maxConnections": "8",
+    "shouldPersist": "true",
+    "showAdvanced": "true",
+    "maxDuration": "30",
+    "minConnections": "1"
+}
+Object.keys(settings).forEach(k => localStorage.setItem(k, settings[k]) )
+    """)
+    driver.get('https://fast.com')
 
     check_count = 0 
     max_check_count = 120
-    speed_value_element = None
+    speed_indicator_element = None
 
-    while speed_value_element is None and check_count < max_check_count:
+    while speed_indicator_element is None and check_count < max_check_count:
         try:
-            speed_value_element = driver.find_element_by_css_selector('#speed-value.succeeded')
+            speed_indicator_element = driver.find_element_by_css_selector('#speed-progress-indicator.succeeded')
         except NoSuchElementException as nse:
             check_count += 1
-            time.sleep(0.2)
+            time.sleep(1)
 
     end = datetime.datetime.now()
 
@@ -56,17 +62,27 @@ def main():
         'duration': str(end-start)
     }
 
-    if speed_value_element is not None:
+    if speed_indicator_element is not None:
         results['status'] = 'success'
         results['speeds'] = {
-            'down': int(speed_value_element.text)
+            'down': float(driver.find_element_by_css_selector('#speed-value').text),
+            'up': float(driver.find_element_by_css_selector('#upload-value').text)
         }
-        print(json.dumps(results, indent=4))
+        results['data'] = {
+            'down': float(driver.find_element_by_css_selector('#down-mb-value').text),
+            'up': float(driver.find_element_by_css_selector('#up-mb-value').text),
+        }
+        results['client'] = {
+            'location': driver.find_element_by_css_selector('#user-location').text,
+            'ip': driver.find_element_by_css_selector('#user-ip').text,
+            'isp': driver.find_element_by_css_selector('#user-isp').text,
+        }
     elif check_count >= max_check_count:
         results['status'] = 'error'
         results['message'] = 'Max check count exceeded.'
         driver.save_screenshot('/output_dir/timeout.png')
 
+    print(json.dumps(results))
     driver.quit()
 
 main()
